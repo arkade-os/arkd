a498e4613ff20c4447eae0cfefb558f956c5803a
altafan <18440657+altafan@users.noreply.github.com>
2025-07-29 22:56:30 +0200

    Fix

--- internal/infrastructure/db/sqlite/vtxo_repo.go
+++ internal/infrastructure/db/sqlite/vtxo_repo.go
@@ -3,6 +3,7 @@ package sqlitedb
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"sort"
 	"strings"
@@ -315,6 +316,9 @@ func (v *vtxoRepository) GetAllVtxosWithPubKeys(
 ) ([]domain.Vtxo, error) {
 	res, err := v.querier.SelectVtxosWithPubkeys(ctx, pubkeys)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
 		return nil, err
 	}
 	rows := make([]queries.VtxoVw, 0, len(res))
