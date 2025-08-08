f720efd0208e85ee187cc2262db74fec704fe463
louisinger <louis@vulpem.com>
2025-08-08 18:10:21 +0200

    fix CLTV decoding

--- pkg/ark-lib/script/closure.go
+++ pkg/ark-lib/script/closure.go
@@ -448,28 +448,25 @@ func (d *CLTVMultisigClosure) Decode(script []byte) (bool, error) {
 		return false, nil
 	}
 
-	var locktime []byte
+	var locktime int32
 	isSmallInt := txscript.IsSmallInt(tokenizer.Opcode())
 	if isSmallInt {
-		locktime = []byte{tokenizer.Opcode()}
+		locktime = int32(tokenizer.Opcode() - txscript.OP_1 + 1)
 	} else {
-		locktime = tokenizer.Data()
+		locktimeValue, err := txscript.MakeScriptNum(tokenizer.Data(), true, 6)
+		if err != nil {
+			return false, err
+		}
+		locktime = locktimeValue.Int32()
+		if locktime > 0 && locktime <= 16 {
+			return false, fmt.Errorf("expected minimal encoding OP_%d for locktime value %d", locktime, locktime)
+		}
 	}
 
 	for _, opCode := range []byte{txscript.OP_CHECKLOCKTIMEVERIFY, txscript.OP_DROP} {
 		if !tokenizer.Next() || tokenizer.Opcode() != opCode {
 			return false, nil
 		}
-	}
-
-	locktimeValue, err := txscript.MakeScriptNum(locktime, true, 6)
-	if err != nil {
-		return false, err
-	}
-	locktimeNumber := locktimeValue.Int32()
-
-	if !isSmallInt && locktimeNumber > 0 && locktimeNumber <= 16 {
-		return false, fmt.Errorf("expected minimal encoding OP_%d for locktime value %d, got %x", locktimeNumber, locktimeNumber, locktime)
 	}
 
 	multisigClosure := &MultisigClosure{}
@@ -483,7 +480,7 @@ func (d *CLTVMultisigClosure) Decode(script []byte) (bool, error) {
 		return false, nil
 	}
 
-	d.Locktime = arklib.AbsoluteLocktime(locktimeValue.Int32())
+	d.Locktime = arklib.AbsoluteLocktime(locktime)
 	d.MultisigClosure = *multisigClosure
 
 	return valid, nil
