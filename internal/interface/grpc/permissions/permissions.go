be717609eee200e5a8565ab359a97af0d74461a2
altafan <18440657+altafan@users.noreply.github.com>
2025-10-03 14:33:11 +0200

    Lint

--- internal/interface/grpc/permissions/permissions.go
+++ internal/interface/grpc/permissions/permissions.go
@@ -284,10 +284,12 @@ func AllPermissionsByMethod() map[string][]bakery.Op {
 			Entity: EntityManager,
 			Action: "read",
 		}},
-		fmt.Sprintf("/%s/GetActiveScriptConvictions", arkv1.AdminService_ServiceDesc.ServiceName): {{
-			Entity: EntityManager,
-			Action: "read",
-		}},
+		fmt.Sprintf("/%s/GetActiveScriptConvictions", arkv1.AdminService_ServiceDesc.ServiceName): {
+			{
+				Entity: EntityManager,
+				Action: "read",
+			},
+		},
 		fmt.Sprintf("/%s/PardonConviction", arkv1.AdminService_ServiceDesc.ServiceName): {{
 			Entity: EntityManager,
 			Action: "write",
