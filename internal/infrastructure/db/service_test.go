200d4b35e79648bacef8cbc9a8329e6864072777
louisinger <41042567+louisinger@users.noreply.github.com>
2025-10-17 16:36:15 +0200

    Fix & Test repo.GetSweepableRounds (#768)

--- internal/infrastructure/db/service_test.go
+++ internal/infrastructure/db/service_test.go
@@ -512,7 +512,7 @@ func testRoundRepository(t *testing.T, svc ports.RepoManager) {
 				},
 				ForfeitTxs:        []domain.ForfeitTx{f1Tx(), f2Tx(), f3Tx(), f4Tx()},
 				FinalCommitmentTx: emptyTx,
-				Timestamp:         now.Add(60 * time.Second).Unix(),
+				Timestamp:         now.Unix(),
 			},
 		}
 		events = append(events, newEvents...)
@@ -545,6 +545,11 @@ func testRoundRepository(t *testing.T, svc ports.RepoManager) {
 		require.NotNil(t, txs)
 		require.Equal(t, 3, len(txs))
 
+		sweepableRounds, err := svc.Rounds().GetSweepableRounds(ctx)
+		require.NoError(t, err)
+		require.Len(t, sweepableRounds, 1)
+		require.Equal(t, commitmentTxid, sweepableRounds[0])
+
 		newEvents = []domain.Event{
 			domain.BatchSwept{
 				RoundEvent: domain.RoundEvent{
@@ -565,6 +570,20 @@ func testRoundRepository(t *testing.T, svc ports.RepoManager) {
 		require.NoError(t, err)
 		require.NotNil(t, roundById)
 		roundsMatch(t, *sweptRound, *roundById)
+
+		roundWithoutVtxoTree := domain.NewRound()
+		roundWithoutVtxoTree.Stage.Code = int(domain.RoundFinalizationStage)
+		roundWithoutVtxoTree.CommitmentTxid = randomString(32)
+		roundWithoutVtxoTree.Stage.Ended = true
+		err = svc.Rounds().AddOrUpdateRound(ctx, *roundWithoutVtxoTree)
+		require.NoError(t, err)
+
+		sweepableRounds, err = svc.Rounds().GetSweepableRounds(ctx)
+		require.NoError(t, err)
+		// check it is empty because:
+		// - first round has been swept
+		// - second round has no vtxo tree
+		require.Empty(t, sweepableRounds)
 	})
 }
 
