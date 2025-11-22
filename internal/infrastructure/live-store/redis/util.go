7a7bc6176a56ee6fcd5ff74e0ee1d2ec00e81439
Dusan Sekulic <sekula87@gmail.com>
2025-11-22 17:21:57 +0100

    pr review

--- internal/infrastructure/live-store/redis/util.go
+++ internal/infrastructure/live-store/redis/util.go
@@ -1,7 +1,14 @@
 package redislivestore
 
+import log "github.com/sirupsen/logrus"
+
 func safeSend(ch chan struct{}) {
-	defer func() { recover() }()
+	defer func() {
+		if r := recover(); r != nil {
+			log.Warnf("safeSend recovered from panic: %v", r)
+		}
+	}()
+
 	if ch != nil {
 		ch <- struct{}{}
 	}
