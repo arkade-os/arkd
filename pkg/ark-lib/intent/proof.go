49b5b61ccc0a148b1ae7a2e4b84b69e4e04e9a6b
Joshua Aruokhai <joshuaaruokhaitech@gmail.com>
2025-11-26 05:59:32 +0100

    Add Asset field to Vtxo struct and IsSeal field to Input struct

--- pkg/ark-lib/intent/proof.go
+++ pkg/ark-lib/intent/proof.go
@@ -45,6 +45,7 @@ type Input struct {
 	OutPoint    *wire.OutPoint
 	Sequence    uint32
 	WitnessUtxo *wire.TxOut
+	IsSeal      bool
 }
 
 // Verify takes an encoded b64 proof tx and a message to validate the proof
