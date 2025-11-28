7adb6fa3c9acdb6f723223923a2d1fc1899798f6
altafan <18440657+altafan@users.noreply.github.com>
2025-11-28 18:47:29 +0100

    Check for offchain tx existance in SubmitOffchainTx

--- internal/core/application/service.go
+++ internal/core/application/service.go
@@ -428,10 +428,12 @@ func (s *service) SubmitOffchainTx(
 			changes = append(changes, change)
 		}
 
-		if err := s.repoManager.Events().Save(
-			ctx, domain.OffchainTxTopic, txid, changes,
-		); err != nil {
-			log.WithError(err).Errorf("failed to save events for offchain tx %s", txid)
+		if len(changes) > 0 {
+			if err := s.repoManager.Events().Save(
+				ctx, domain.OffchainTxTopic, txid, changes,
+			); err != nil {
+				log.WithError(err).Errorf("failed to save events for offchain tx %s", txid)
+			}
 		}
 	}()
 
@@ -470,6 +472,19 @@ func (s *service) SubmitOffchainTx(
 
 		checkpointTxsByVtxoKey[vtxoKey] = txid
 		spentVtxoKeys = append(spentVtxoKeys, vtxoKey)
+	}
+
+	existingOffchainTx, err := s.repoManager.OffchainTxs().GetOffchainTx(ctx, txid)
+	if err != nil {
+		log.WithError(err).Errorf("failed to fetch offchain tx %s", txid)
+		return nil, errors.INTERNAL_ERROR.New("something wen wrong").
+			WithMetadata(map[string]any{"txid": txid})
+	}
+
+	if existingOffchainTx != nil {
+		return nil, errors.INVALID_ARK_PSBT.New(
+			"duplicated offchain tx %s", txid,
+		).WithMetadata(errors.PsbtMetadata{Tx: signedArkTx})
 	}
 
 	event, err := offchainTx.Request(txid, signedArkTx, checkpointTxs)
