3ff371807ead25d5e911b70a906d7d2f821e5db0
altafan <18440657+altafan@users.noreply.github.com>
2025-12-30 15:30:57 +0100

    Fix arkd-wallet launcher

--- cmd/arkd-wallet/main.go
+++ cmd/arkd-wallet/main.go
@@ -38,7 +38,9 @@ func main() {
 	log.RegisterExitHandler(svc.Stop)
 
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT, os.Interrupt)
+	signal.Notify(
+		sigChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGHUP, os.Interrupt,
+	)
 	<-sigChan
 
 	log.Info("shutting down service...")
