d7913e9d98781fae74ef5bf1c78b5f784b8b77ec
Bob Smith <5396652+bitcoin-coder-bob@users.noreply.github.com>
2026-01-06 11:38:35 -0500

    fix postgres/sqlite migrations for views

--- internal/infrastructure/db/postgres/migration/20260106000000_add_vtxo_updated_at.up.sql
+++ internal/infrastructure/db/postgres/migration/20260106000000_add_vtxo_updated_at.up.sql
@@ -8,6 +8,7 @@ WHERE updated_at IS NULL;
 
 
 -- update the vtxo_vw to include the new updated_at column
+DROP VIEW IF EXISTS intent_with_inputs_vw;
 DROP VIEW IF EXISTS vtxo_vw;
 CREATE VIEW vtxo_vw AS
 SELECT v.*, string_agg(vc.commitment_txid, ',') AS commitments
@@ -15,3 +16,10 @@ FROM vtxo v
 LEFT JOIN vtxo_commitment_txid vc
 ON v.txid = vc.vtxo_txid AND v.vout = vc.vtxo_vout
 GROUP BY v.txid, v.vout;
+
+
+CREATE VIEW intent_with_inputs_vw AS
+SELECT vtxo_vw.*, intent.*
+FROM intent
+LEFT OUTER JOIN vtxo_vw
+ON intent.id = vtxo_vw.intent_id;
--- internal/infrastructure/db/sqlite/migration/20260106000000_add_vtxo_updated_at.up.sql
+++ internal/infrastructure/db/sqlite/migration/20260106000000_add_vtxo_updated_at.up.sql
@@ -7,10 +7,18 @@ SET updated_at = (CAST((strftime('%s','now') || substr(strftime('%f','now'),4,3)
 WHERE updated_at IS NULL;
 
 -- update the vtxo_vw to include the new updated_at column
+DROP VIEW IF EXISTS intent_with_inputs_vw;
 DROP VIEW IF EXISTS vtxo_vw;
 CREATE VIEW vtxo_vw AS
-SELECT v.*, group_concat(vc.commitment_txid, ',') AS commitments
+SELECT v.*, COALESCE(group_concat(vc.commitment_txid), '') AS commitments
 FROM vtxo v
 LEFT JOIN vtxo_commitment_txid vc
-  ON v.txid = vc.vtxo_txid AND v.vout = vc.vtxo_vout
+ON v.txid = vc.vtxo_txid AND v.vout = vc.vtxo_vout
 GROUP BY v.txid, v.vout;
+
+
+CREATE VIEW intent_with_inputs_vw AS
+SELECT vtxo_vw.*, intent.*
+FROM intent
+LEFT OUTER JOIN vtxo_vw
+ON intent.id = vtxo_vw.intent_id;
