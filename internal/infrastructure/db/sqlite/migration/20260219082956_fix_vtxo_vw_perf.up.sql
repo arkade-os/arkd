5ad49ebb0b069a95ab86a176699243c7c658e200
altafan <18440657+altafan@users.noreply.github.com>
2026-02-19 16:23:46 +0100

    Clean migration

--- internal/infrastructure/db/sqlite/migration/20260219082956_fix_vtxo_vw_perf.up.sql
+++ internal/infrastructure/db/sqlite/migration/20260219082956_fix_vtxo_vw_perf.up.sql
@@ -1,22 +1,4 @@
 DROP VIEW IF EXISTS intent_with_inputs_vw;
-DROP VIEW IF EXISTS round_intents_vw;
-DROP VIEW IF EXISTS vtxo_vw;
-
-CREATE VIEW vtxo_vw AS
-SELECT
-  v.*,
-  COALESCE((
-    SELECT group_concat(vc.commitment_txid)
-    FROM vtxo_commitment_txid vc
-    WHERE vc.vtxo_txid = v.txid AND vc.vtxo_vout = v.vout
-  ), '') AS commitments,
-  COALESCE(ap.asset_id, '') AS asset_id,
-  COALESCE(ap.amount, 0) AS asset_amount
-FROM vtxo v
-LEFT JOIN (
-  SELECT DISTINCT txid, vout, asset_id, amount
-  FROM asset_projection
-) AS ap ON ap.txid = v.txid AND ap.vout = v.vout;
 
 CREATE VIEW intent_with_inputs_vw AS
 SELECT
@@ -54,6 +36,3 @@ LEFT JOIN asset_projection ap ON v.txid = ap.txid AND v.vout = ap.vout;
 
 CREATE INDEX IF NOT EXISTS idx_asset_projection_vtxo ON asset_projection(txid, vout);
 
-CREATE VIEW round_intents_vw AS
-SELECT intent.*
-FROM intent;
