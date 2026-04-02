c094527e164b30f4d55c227970ddf3b01acde7c5
Bob Smith <5396652+bitcoin-coder-bob@users.noreply.github.com>
2026-04-02 13:42:28 -0400

    lint

--- internal/config/config.go
+++ internal/config/config.go
@@ -258,20 +258,15 @@ var (
 	defaultNoTLS               = true
 	defaultAllowCSVBlockType   = false
 
-	defaultRoundMaxParticipantsCount     = 128
-	defaultRoundMinParticipantsCount     = 1
-	defaultOtelPushInterval              = 10 // seconds
-	defaultHeartbeatInterval             = 60 // seconds
-	defaultRoundReportServiceEnabled     = false
-	defaultSettlementMinExpiryGap        = 0 // disabled by default
-	defaultMaxTxWeight                   = int64(0.01 * bitcoinBlockWeight)
-	defaultAssetTxMaxWeightRatio         = 0.5
-	defaultVtxoNoCsvValidationCutoffDate = 0 // disabled by default
-	defaultEnablePprof                   = false
-	defaultIndexerExposure               = "public"
-	defaultIndexerAuthTokenExpiry        = 300 // 5 minutes in seconds
-	defaultMaxConcurrentStreams          = uint32(1000)
-	defaultMaxOpReturnOuts               = uint32(3)
+	defaultOtelPushInterval          = 10 // seconds
+	defaultHeartbeatInterval         = 60 // seconds
+	defaultRoundReportServiceEnabled = false
+	defaultAssetTxMaxWeightRatio     = 0.5
+	defaultEnablePprof               = false
+	defaultIndexerExposure           = "public"
+	defaultIndexerAuthTokenExpiry    = 300 // 5 minutes in seconds
+	defaultMaxConcurrentStreams      = uint32(1000)
+	defaultMaxOpReturnOuts           = uint32(3)
 )
 
 func LoadConfig() (*Config, error) {
