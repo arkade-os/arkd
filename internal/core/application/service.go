26e3e84f2eaafa6e9da309584ee8152380ba810d
Pietralberto Mazza <18440657+altafan@users.noreply.github.com>
2026-04-27 13:26:21 +0200

    Wait for projection db updates before sending events through streams (#1038)
    * Wait for projection db updates before sending events through streams
    
    * Fixes after review
    
    * Lint

--- internal/core/application/service.go
+++ internal/core/application/service.go
@@ -300,11 +300,11 @@ func (s *service) Start() error {
 }
 
 func (s *service) registerEventHandlers() {
-	s.repoManager.Events().RegisterEventsHandler(
-		domain.RoundTopic, func(events []domain.Event) {
-			round := domain.NewRoundFromEvents(events)
+	s.repoManager.RegisterBatchUpdateHandler(
+		func(round domain.Round) {
 			go s.propagateEvents(context.Background(), round)
 
+			events := round.Events()
 			lastEvent := events[len(events)-1]
 			if lastEvent.GetType() == domain.EventTypeBatchSwept {
 				batchSweptEvent := lastEvent.(domain.BatchSwept)
@@ -351,15 +351,13 @@ func (s *service) registerEventHandlers() {
 		},
 	)
 
-	s.repoManager.Events().RegisterEventsHandler(
-		domain.OffchainTxTopic, func(events []domain.Event) {
-			offchainTx := domain.NewOffchainTxFromEvents(events)
-
+	s.repoManager.RegisterOffchainTxUpdateHandler(
+		func(offchainTx domain.OffchainTx) {
 			if !offchainTx.IsFinalized() {
 				return
 			}
 
-			txid, spentVtxoKeys, newVtxos, err := decodeTx(*offchainTx)
+			txid, spentVtxoKeys, newVtxos, err := decodeTx(offchainTx)
 			if err != nil {
 				log.WithError(err).Warn("failed to decode offchain tx")
 				return
@@ -3414,7 +3412,7 @@ func (s *service) listenToScannerNotifications() {
 	}
 }
 
-func (s *service) propagateEvents(ctx context.Context, round *domain.Round) {
+func (s *service) propagateEvents(ctx context.Context, round domain.Round) {
 	lastEvent := round.Events()[len(round.Events())-1]
 	events := make([]domain.Event, 0)
 	switch ev := lastEvent.(type) {
@@ -3538,7 +3536,7 @@ func (s *service) propagateRoundSigningNoncesGeneratedEvent(
 	s.eventsCh <- events
 }
 
-func (s *service) scheduleSweepBatchOutput(round *domain.Round) {
+func (s *service) scheduleSweepBatchOutput(round domain.Round) {
 	// Schedule the sweeping procedure only for completed round.
 	if !round.IsEnded() {
 		return
